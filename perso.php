01886232555dbbd99b43a90e4602f5c5a454c5d4
IEDIN2021 <85932847+IEDIN2021@users.noreply.github.com>
2021-09-13 14:33:38 +0200

    updates

--- perso.php
+++ perso.php
@@ -8,17 +8,33 @@ abstract class Perso {
     protected $armeEquipe = null;
     protected $armes = [];
 
-    public function __construct($arme) {
+   /* public function __construct($arme) {
         $this->arme = $arme;
-    }
+    }*/
 }
 
 class Hero extends Perso {
     protected $ptExp = null;
 
+    function __construct() {
+        $this->ptVie = 40;
+        $this->ptEndurance = 46;
+        $this->sous = 46;
+        $this->ptForce = 46;
+        $this->armeEquipe = "epe";
+      }
+
 }
 
 class Monstre extends Perso {
+
+    function __construct() {
+        $this->ptVie = 50;
+        $this->ptEndurance = 35;
+//        $this->sous = 46;
+        $this->ptForce = 35;
+        $this->armeEquipe = "epe";
+      }
 
 }
 
@@ -27,17 +43,15 @@ class Pnj {
     private $armes = [];
 }
 class Arme {
-    private $nomArme = null;
-    private $ptFor = null;
-    private $prix = null;
-    //protected $armes = [];
+    public $ptFor = null;
+    public $prix = null;
 }
 // defined weapons
-$epe = new Arme("Epe",9,15);
-$couteau = new Arme("Couteau",5,3);
-$ak47 = new Arme("AK47",28,25);
-$m16 = new Arme("M16",25,30);
-$m84 = new Arme("M84",150,50);
+$epe = new Arme(9,15);
+$couteau = new Arme(5,3);
+$ak47 = new Arme(28,25);
+$m16 = new Arme(25,30);
+$m84 = new Arme(150,50);
 
 $tableauArmes = [$epe, $couteau,$ak47, $m16, $m84];
 $armeSelectionnee = rand(0, count($tableauArmes) - 1);
@@ -52,7 +66,13 @@ $armeSelectionnee = rand(0, count($tableauArmes) - 1);
 
 
 
-$hero = new Hero($epe);
+//$hero = new Hero ();
+    
+$hero = new hero();
+$monstre = new monstre();
 
-var_dump($hero);
-$hero->Arme;
+
+var_dump($monstre);
+//$hero->Arme;
+
+?>
