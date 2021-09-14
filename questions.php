76cebcbe20d94072d6c18f01e6cd52df65e40fcf
IEDIN2021 <85932847+IEDIN2021@users.noreply.github.com>
2021-09-14 17:14:08 +0200

    Update questions.php

--- questions.php
+++ questions.php
@@ -2,20 +2,24 @@
 //création des objets monstre
 $question = [];
 // oui - combat ou fuite game over
-$question[] = ("Ta envie de combattre ?");
+$question[] = "Ta envie de combattre ?";
 //si oui -> PNJ  / non game over
-$question[] = ("Ta besoin d'une nouvelle arme? ");
+//$question[] = "Ta besoin d'une nouvelle arme? ";
 //si oui -> PNJ  / si non combat
-$question[] = ("Prends une nouvelle arme du PNJ? ");
+//$question[] = "Prends une nouvelle arme du PNJ? ";
 //si oui -> combat  / si non PNJ
-$question[] = ("Attaque avec ton arme actuelle !");
+$question[] = "Attaque avec ton arme actuelle !";
 //si oui -> Game over  / si non combat
-$question[] = ("Je refuse de combattre !");
+$question[] = "Je refuse de combattre !";
 
-$randomquestions = rand(0,4);
+$randomquestions = rand(0,2);
 
 $displayquestions = $question[$randomquestions];
 
 //var_dump($displayquestions);
 
 //var_dump($question[$randomquestions]);
+
+if (isset($_POST['go'])) {
+
+}
